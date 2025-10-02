3b0a0d717f73350abb92f5b31be74e6481d8722e
Abhijit1102 <abhijitrajkumar2@gmail.com>
2025-10-02 12:50:32 +0530

    updated

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
 from excel_analyser import excel_analyser_app
 from emi_calculator import emi_calculator_app
-from quiz_generator import quiz_generator_app
 
 # Sidebar page selector
 page = st.sidebar.selectbox("Select App", ["Excel/CSV Analyzer", "EMI Calculator", "Quiz app"])
@@ -20,5 +19,4 @@ if page == "Excel/CSV Analyzer":
     excel_analyser_app()
 elif page == "EMI Calculator":
     emi_calculator_app()
-else:
-    quiz_generator_app()
+
